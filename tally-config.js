ff846c4d1e64159283182c4354480e47e6d6f201
Nathan Castro <nathanslc18@gmail.com>
2025-07-01 16:56:27 -0300

    Create tally-config.js

---
+++ tally-config.js
@@ -0,0 +1,18 @@
+<script>
+  TallyConfig = {
+    formId: "wbMD9E",
+    popup: {
+      emoji: {
+        text: "👋",
+        animation: "wave"
+      },
+      open: {
+        trigger: "time",
+        ms: 5000
+      },
+      autoClose: 0,
+      showOnce: true,
+      doNotShowAfterSubmit: true
+    }
+  };
+</script>
